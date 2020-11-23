8ecc3e7b2e38285a6ce6a3fe44cd8369c9cc658d
Jimmy <jding14@stevens.edu>
2020-11-23 11:44:30 +0800

    [Jiaming Ding]Add recommendation system

--- routes/home.js
+++ routes/home.js
@@ -2,10 +2,26 @@ const express = require('express');
 const router = express.Router();
 const moviesData = require('../data/movies');
 
+function orderSort(obj1, obj2) {
+    var a = obj1.rating;
+    var b = obj2.rating;
+    if (a > b) {
+        return -1
+    } else if (a < b) {
+        return 1
+    } else {
+        return 0
+    }
+};
+
 router.get('/', async (req, res) => {
     try {
         let movieList = await moviesData.getAllMovies();
-        res.render('pages/home', { movieList: movieList, title: "Home"});
+        
+        var ratingList = JSON.parse(JSON.stringify(movieList));
+        ratingList.sort(orderSort);
+
+        res.render('pages/home', { movieList: movieList, ratingList:ratingList, title: "Home" });
     } catch (e) {
         res.status(404).json({ message: e });
     }
